1eb3dc54d8393eba212ff3f2a008906ca2728211
Agustin Vignolo <agustinvignolo@MacBook-Air-de-Agustin.local>
2020-05-29 02:10:41 -0300

    Updates

---
+++ precache-manifest.ee836c4c85bbeccaed745a60ffd973d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7227b639fd1c441600c427ea10b8031c",
+    "url": "/react-cotizador/index.html"
+  },
+  {
+    "revision": "b50eb29da595cc028be1",
+    "url": "/react-cotizador/static/css/main.a1b4fa64.chunk.css"
+  },
+  {
+    "revision": "52dc6eef15867692c41b",
+    "url": "/react-cotizador/static/js/2.ea90b46e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cotizador/static/js/2.ea90b46e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b50eb29da595cc028be1",
+    "url": "/react-cotizador/static/js/main.d2229111.chunk.js"
+  },
+  {
+    "revision": "697109f4991a5344183c",
+    "url": "/react-cotizador/static/js/runtime-main.7b5ba6cc.js"
+  }
+]);
